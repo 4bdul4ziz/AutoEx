e46b1e9e9e277f0b6554e787857dbef7a866a572
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-10-03 04:35:50 +0530

    frontend application for the studio framework

---
+++ R/AutoEx_interface.R
@@ -0,0 +1,75 @@
+library(shiny)
+library(shinythemes)
+
+ui <- fluidPage(theme = shinytheme("cerulean"),
+                titlePanel("Auto EX"),
+                sidebarLayout(position = "left",
+                  sidebarPanel(
+                    tags$h3("Keyword  Input:"),
+                    textInput("keyword_input", "Enter the keywords:", "")
+                  ),
+                  
+                  mainPanel(
+                    h1("Selected Text"),
+                    #h4("The keywords matched and the weightage associated is: "),
+                    #textInput("string_data","Enter the string: "),
+                    #string_data <- ("This is a sample sentence"),
+                    string_data <-read.delim("testing.txt"),
+                    h3(textOutput(string_data))
+                  )
+                ),
+                sidebarLayout(
+                  sidebarPanel(
+                    tags$b("Keyword Count"),
+                    textOutput("keyword_output")
+                  ),
+                  mainPanel()
+                ),
+                sidebarLayout(
+                  sidebarPanel(
+                    tags$h4("Marks calcuated based on weightage are: "),
+                    textOutput("marks_output")
+                  ),
+                  mainPanel()
+                ),
+                sidebarLayout(
+                  sidebarPanel(
+                    fileInput("input_txt", "Choose the txt file",
+                              multiple = FALSE,
+                              accept = c("text/plain",
+                                         ".txt", "space")),
+                    
+                    # Horizontal line ----
+                    tags$hr()
+                  ),  
+                  mainPanel()
+                  
+                )
+                
+          
+)
+
+server <- function(input, output) {
+  
+  output$keyword_output <- renderText({
+    paste( keywords ) #functions 
+  })
+  output$marks_output <- renderText({
+    paste( marks ) #functions 
+  })
+  
+  keyword_count <- function(string_data) {
+    
+  return(keywords)
+  }
+  
+  calculate_marks <-function(keywords){
+    
+    return(marks)
+  }
+  #output$string_data <- read.delim("input_txt.txt")
+} # server
+
+
+# Create Shiny object
+shinyApp(ui = ui, server = server)
