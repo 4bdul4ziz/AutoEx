659cb505e78abcfe6005a2d5e501d8fd633bf032
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-10-03 03:58:55 +0530

    restructuring

--- tempCodeRunnerFile.R
+++
@@ -1,8 +0,0 @@
-library(keras)
-library(tensorflow)
-library(reticulate)
-mnist <- dataset_mnist()
-x_train <- mnist$train$x
-y_train <- mnist$train$y
-x_test <- mnist$test$x
-y_test <- mnist$test$y
