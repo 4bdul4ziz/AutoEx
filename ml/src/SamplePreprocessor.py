5605bfea6d849d3482958bfc038b05a0f108399a
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-10-04 19:41:38 +0530

    added ricker filter funcs

--- ml/src/SamplePreprocessor.py
+++ ml/src/SamplePreprocessor.py
@@ -20,20 +20,52 @@ def preprocess(img, imgSize, dataAugmentation=False):
         wStretched = max(int(img.shape[1] * (1 + stretch)), 1)  # random width, but at least 1
         img = cv2.resize(img, (wStretched, img.shape[0]))  # stretch horizontally by factor 0.5 .. 1.5
 
+    #TO-DO
+    #hybrid stuff
+
+
     # create target image and copy sample image into it
     (wt, ht) = imgSize
     (h, w) = img.shape
-    fx = w / wt
+    fx = w / wt 
     fy = h / ht
     f = max(fx, fy)
     newSize = (max(min(wt, int(w / f)), 1),
                max(min(ht, int(h / f)), 1))  # scale according to f (result at least 1 and at most wt or ht)
-    img = cv2.resize(img, newSize)
+    img = cv2.resize(img, newSize)      #i have no idea why am i resizing the image here but if i dont its fucked up
     target = np.ones([ht, wt]) * 255
     target[0:newSize[1], 0:newSize[0]] = img
 
     # transpose for TF
     img = cv2.transpose(target)
+
+    #ricker wavelet
+    def rickerWavelet(x, sigma):
+        return (1 - (x ** 2) / (sigma ** 2)) * np.exp(-(x ** 2) / (2 * (sigma ** 2)))
+
+    def rickerFilter(size, sigma):
+        # make sure size is odd
+        size = size if size % 2 == 1 else size + 1
+
+        # create ricker filter
+        x = np.arange(0, size) - (size - 1) / 2
+        filter = rickerWavelet(x, sigma)
+
+        # normalize filter
+        filter /= np.sum(filter)
+
+        return filter
+    
+    def applyRickerFilter(img, size, sigma):
+        # create filter
+        filter = rickerFilter(size, sigma)
+
+        # apply filter to image
+        imgFiltered = cv2.filter2D(img, -1, filter)
+
+        return imgFiltered
+
+    img = applyRickerFilter(img, 5, 1)
 
     # normalize
     (m, s) = cv2.meanStdDev(img)
