8f82ede082edab0c174ef826495b80d6fc221cd5
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-11-14 11:06:17 +0530

    interface beta v1.2

--- R/AutoEx_interface.R
+++ R/AutoEx_interface.R
@@ -1,5 +1,15 @@
+
+#UI R - interface [ui.R] frontend
+#Server function [server.R] backend
+#shinyApp function [fuses both ui and server]
+
+#flow of i/o
+#input -> ui -> server ->[process analyze data]
+#output <- ui <- server <- [processed data]
+
 library(shiny)
 library(shinythemes)
+source_python("/Users/samik/Desktop/Programming/AutoEx/test.py")
 
 ui <- fluidPage(theme = shinytheme("cerulean"),
                 titlePanel("Auto EX"),
@@ -10,10 +20,13 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                   ),
                   
                   mainPanel(
-                    h1("Selected Text"),
+                    h3("NLP pipeline output Text"),
                     #h4("The keywords matched and the weightage associated is: "),
                     #textInput("string_data","Enter the string: "),
-                    textOutput("string_data")
+                    textOutput("string_data"),
+                    h3("The examiner entered keywords list: "),
+                    textOutput("examiner_list"),
+                    
                   )
                 ),
                 sidebarLayout(
@@ -40,8 +53,14 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                     # Horizontal line ----
                     tags$hr()
                   ),  
-                  mainPanel()
+                  mainPanel(
                   
+                  )
+                #actionButton(parameter<-script, label, icon = NULL, width = NULL, ...). 
+                #here we will be having the executing button for executing the script of the model and 
+                #output will be the text file of handwritten words
+                
+                #actionButton(function<-weightage calculation, label, icon = NULL, width = NULL, ...)
                 )
     
           
@@ -53,9 +72,11 @@ server <- function(input, output) {
     paste( keywords ) #functions 
   })
   output$marks_output <- renderText({
+    marks <-function1()
     paste( marks ) #functions 
   })
-  
+  #functions using algorithm that will award the marks based on examiners choice 
+  #under development
   keyword_count <- function(string_data) {
     
   return(keywords)
@@ -65,14 +86,27 @@ server <- function(input, output) {
     
     return(marks)
   }
-  reading_data <-read.table("testing.txt",              # TXT data file indicated as string or full path to the file
+  
+  
+
+  
+  
+  reading_data <-read.table("input_txt.txt",              # TXT data file indicated as string or full path to the file
                             header = FALSE,    # Whether to display the header (TRUE) or not (FALSE)
                             sep = "",          # Separator of the columns of the file
                             dec = ".")         # Character used to separate decimals of the numbers in the file
   output$string_data <- renderText({
     paste(reading_data) #functions 
   })
-
+  
+  list_of_keywords_by_examiner <-read.table("examiner_list.txt",
+                             header= FALSE,
+                             sep= "",
+                             dec =".")
+  output$examiner_list <-renderText({
+    paste(list_of_keywords_by_examiner)
+  })
+  
     
 } # server
 
